8f8db5cfca8f5d7367446c61eaa308fff10d95a7
KowalewskiPawel <pawelkowalewsk@gmail.com>
2023-12-11 22:28:33 +0100

    feat!: implement CurrencyInputForm

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,9 +1,10 @@
-import { RatesTable, RefreshButton } from "../../components";
+import { CurrencyInputForm, RatesTable, RefreshButton } from "../../components";
 import "../../styles/global.scss";
 
 export const Main = () => (
   <div className="mainContainer">
     <h1>Exchange Rate</h1>
+    <CurrencyInputForm />
     <RefreshButton />
     <RatesTable />
   </div>
